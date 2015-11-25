70fedd2783a8e310f8be0bd29e520cf54de62088
Jake Robers <jakerobers@gmail.com>
2015-11-24 20:21:24 -0600

    idk what i did

--- generators/app/templates/assets/applications/mobile/app.mobile.js
+++ generators/app/templates/assets/applications/mobile/app.mobile.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-angular.module('app', ['ngRoute', 'ngResource', 'ngMessages', 'ngWebSocket', 'app.dashboard'])
 angular.module('app.dashboard', []);
+angular.module('app', ['ngRoute', 'ngResource', 'ngMessages', 'ngWebSocket', 'app.dashboard'])
 
 
 .config(['$httpProvider', function($httpProvider) {
